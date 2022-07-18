b7236d556466698f4fcf0cee48ff8bc1913e4dca
Abdujalilc <abdujalilc@gmail.com>
2022-07-18 11:28:04 +0500

    project added

---
+++ LocalizationApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using LocalizationApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace LocalizationApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetCulture(string code = "")
+        {
+            if (!String.IsNullOrEmpty(code))
+            {
+                CultureInfo.CurrentCulture = new CultureInfo(code);
+                CultureInfo.CurrentUICulture = new CultureInfo(code);
+            }
+            return $"CurrentCulture:{CultureInfo.CurrentCulture.Name}, CurrentUICulture:{CultureInfo.CurrentUICulture.Name}";
+        }
+    }
+}
---
+++ LocalizationApp/CultureMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace LocalizationApp
+{
+    public class CultureMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CultureMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var lang = context.Request.Query["lang"].ToString();
+            if (!string.IsNullOrEmpty(lang))
+            {
+                try
+                {
+                    CultureInfo.CurrentCulture = new CultureInfo(lang);
+                    CultureInfo.CurrentUICulture = new CultureInfo(lang);
+                }
+                catch (CultureNotFoundException) { }
+            }
+            await _next.Invoke(context);
+        }
+    }
+
+    public static class CultureExtensions
+    {
+        public static IApplicationBuilder UseCulture(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CultureMiddleware>();
+        }
+    }
+}
