54851d86e1f7a62f8f558f4713d8d88fc2e7bf26
Abdujalilc <abdujalilc@gmail.com>
2022-07-19 12:04:41 +0500

    localization resources added

---
+++ LocalizationResources/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using LocalizationResources.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System.Diagnostics;
+
+namespace LocalizationResources.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStringLocalizer<HomeController> _localizer;
+        public HomeController(IStringLocalizer<HomeController> localizer)
+        {
+            _localizer = localizer;
+        }
+        public IActionResult Index()
+        {
+            ViewData["Title"] = _localizer["Header"];
+            ViewData["Message"] = _localizer["Message"];
+            return View();
+        }
+    }
+}
---
+++ LocalizationResources/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseDeveloperExceptionPage();
+
+var supportedCultures = new[]
+{
+                new CultureInfo("en"),
+                new CultureInfo("ru"),
+                new CultureInfo("de")
+            };
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("ru"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+});
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
